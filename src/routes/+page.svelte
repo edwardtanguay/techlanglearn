bb83ac8a18dfa0fbabf0f5c03ef853126a1da0ae
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-29 00:36:16 +0100

    some style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>Tech Language Learning</h1>
-<p>On this site I record my learning of tech topics in various languages</p>
+<main class="p-6">
+	<header class="mb-6">
+		<h1 class="main">Tech Language Learning</h1>
+		<p>On this site I record my learning of tech topics in various languages</p>
+	</header>
 
-<div class="markdown">
-	{@html $page.data.content}
-</div>
+	<div class="markdown">
+		{@html $page.data.content}
+	</div>
+</main>
