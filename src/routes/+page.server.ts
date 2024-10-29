6165c7330bd28bcf644f6fd3ce7e5b89b9e65c0e
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-29 01:08:04 +0100

    changed logs

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,13 @@
-// import fs from 'fs';
-// import path from 'path';
 import { marked } from 'marked';
 import { PUBLIC_BASE_URL } from '$env/static/public'; // Import your base URL
 
 console.log(111, PUBLIC_BASE_URL);
 
 export async function load() {
+	// KEEP: CODE THAT LOADS FROM FILE SYSTEM INSTEAD OF FETCHING FROM STATIC FILE
 	// const filePath = path.join(process.cwd(), 'src', 'data', 'svelte5GuideForBeginners.md');
 	// const markdown = fs.readFileSync(filePath, 'utf-8');
+
 	const fileUrl = `${PUBLIC_BASE_URL}/data/svelte5GuideForBeginners.md`;
 	const res = await fetch(fileUrl);
 	const markdown = await res.text();
