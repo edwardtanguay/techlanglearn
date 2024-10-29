0f68f44bc8d5867e16a1b1ec36c4e700ef6d274c
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-29 01:02:00 +0100

    loading files

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,16 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 import { marked } from 'marked';
+import { PUBLIC_BASE_URL } from '$env/static/public'; // Import your base URL
+
+console.log(111, PUBLIC_BASE_URL);
 
 export async function load() {
-	const filePath = path.join(process.cwd(), 'src', 'data', 'svelte5GuideForBeginners.md');
-	const markdown = fs.readFileSync(filePath, 'utf-8');
+	// const filePath = path.join(process.cwd(), 'src', 'data', 'svelte5GuideForBeginners.md');
+	// const markdown = fs.readFileSync(filePath, 'utf-8');
+	const fileUrl = `${PUBLIC_BASE_URL}/data/svelte5GuideForBeginners.md`;
+	const res = await fetch(fileUrl);
+	const markdown = await res.text();
 	const content = marked(markdown);
 	return { content };
 }
