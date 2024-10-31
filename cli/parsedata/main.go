4f11d31d92894a836fa7c5d74ff31f58da504b1f
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-31 09:35:22 +0100

    basic pd test

--- cli/parsedata/main.go
+++ cli/parsedata/main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
-	"os"
 )
 
 func main() {
 	var rootCmd = &cobra.Command{
-		Use:   "helloworld",
-		Short: "Prints Hello, World!",
+		Use:   "parsedata",
+		Short: "Reads the .md files and parses them into JSON files that contain data from the md files, plus extended HTML to be displayed on the site.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello, World!")
+			fmt.Printf("test parsedata")
 		},
 	}
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
 	}
 }
