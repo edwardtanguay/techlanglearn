ef438471d33cb6eefa2d3559ecaa6d71c345c4d9
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-01 22:02:09 +0100

    parser in

--- cli/parsedata/main.go
+++ cli/parsedata/main.go
@@ -11,7 +11,7 @@ func main() {
 		Use:   "parsedata",
 		Short: "Reads the .md files and parses them into JSON files that contain data from the md files, plus extended HTML to be displayed on the site.",
 		Run: func(cmd *cobra.Command, args []string) {
-			parse()
+			parse("../../static/data")
 		},
 	}
 
--- cli/parsedata/parseManager.go
+++ cli/parsedata/parseManager.go
@@ -5,10 +5,17 @@ import (
 	"path/filepath"
 )
 
-func parse() {
+/*
+Parses the md files into JSON for the website to consume
+
+parse("../../static/data")
+
+- use relative path
+*/
+func parse(directory string) {
 	fmt.Printf("This script will parse these files:\n\n")
 
-	mdPathAndFileNames, _ := getFilesFromDirectory("../../static/data", "md")
+	mdPathAndFileNames, _ := getFilesFromDirectory(directory, "md")
 	for _, mdPathAndFileName := range mdPathAndFileNames {
 		lines := getLinesFromFile(mdPathAndFileName)
 		fileName := filepath.Base(mdPathAndFileName)
