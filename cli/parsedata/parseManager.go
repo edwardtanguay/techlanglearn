81e6cec3fe03c63339e0fc40458fad61d7d2990c
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-02 07:54:57 +0100

    cleaned

--- cli/parsedata/parseManager.go
+++ cli/parsedata/parseManager.go
@@ -16,20 +16,16 @@ parse("../../static/data")
 func parse(directory string) {
 	fmt.Printf("This script will parse these files:\n\n")
 
-	// var flashcards []Flashcard
-
 	mdPathAndFileNames, _ := getFilesFromDirectory(directory, "md")
 	for _, mdPathAndFileName := range mdPathAndFileNames {
 
 		fileName := filepath.Base(mdPathAndFileName)
 		fmt.Printf("FILE: %s\n", fileName)
-		fmt.Println("=====================================================")
 
 		lines := getLinesFromFile(mdPathAndFileName)
 		flashcards, _ := getFlashcards(lines)
 
 		fmt.Println("=====================================================")
-		fmt.Printf("Number of lines: %v\n", len(lines))
 		fmt.Printf("Number of flashcards: %v\n", len(flashcards))
 		fmt.Printf("=====================================================\n\n")
 
@@ -37,14 +33,16 @@ func parse(directory string) {
 }
 
 func getFlashcards(lines []string) ([]Flashcard, error) {
+
 	foundFirstBackticks := false
 	foundVocab := false
 	flashcards := []Flashcard{}
 	flashcardLines := []string{}
+
+	// get slice of all lines that have to do with flashcards
 	for _, line := range lines {
 		if foundVocab && foundFirstBackticks {
 			flashcardLines = append(flashcardLines, line)
-			// fmt.Printf("RECORDING FLASHCARD, there are %v flashcard lines now\n", len(flashcardLines))
 		}
 		if strings.Contains(line, "## VOCAB") {
 			foundVocab = true
@@ -53,30 +51,24 @@ func getFlashcards(lines []string) ([]Flashcard, error) {
 			foundFirstBackticks = true
 		}
 	}
-	fmt.Printf("FINISHED: there are %v flashcard lines now\n", len(flashcardLines))
 
+	// create slice of flashcard instances
+	processingLineType := "front"
+	front := ""
+	back := ""
 	for _, flashcardLine := range flashcardLines {
-		fmt.Printf("LINE: %s\n", flashcardLine)
+		switch processingLineType {
+		case "front":
+			front = flashcardLine
+			processingLineType = "back"
+		case "back":
+			back = flashcardLine
+			processingLineType = "BLANK"
+		case "BLANK":
+			flashcards = append(flashcards, Flashcard{front, back})
+			processingLineType = "front"
+		}
+
 	}
-
-	// // create data
-	// processingLineType := "front"
-	// front := ""
-	// back := ""
-	// // fileName := filepath.Base(mdPathAndFileName)
-	// for _, flashcardLine := range flashcardLines {
-	// 	switch processingLineType {
-	// 	case "front":
-	// 		front = flashcardLine
-	// 		processingLineType = "back"
-	// 	case "back":
-	// 		back = flashcardLine
-	// 		processingLineType = "BLANK"
-	// 	case "BLANK":
-	// 		flashcards = append(flashcards, Flashcard{front, back})
-	// 		processingLineType = "front"
-	// 	}
-
-	// }
 	return flashcards, nil
 }
