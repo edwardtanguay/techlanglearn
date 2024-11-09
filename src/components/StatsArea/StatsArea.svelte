1bdb8696a5dd0e2e1b5b171eb1dcee89cce0b1eb
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-09 19:29:08 +0100

    finished

--- src/components/StatsArea/StatsArea.svelte
+++ src/components/StatsArea/StatsArea.svelte
@@ -9,12 +9,12 @@ const flashcards = dataModel.getFlashcards()
 
 <section class="statsArea w-fit rounded bg-slate-200 border-2 border-slate-300 p-6 font-mono">
 	<p>Total days: <span class="highlight">{stats.totalDays}</span></p>
-	<p>Average time per day watching tutorials: <span class="highlight bg-yellow-200 px-1">{stats.averageDurationPerDay}</span></p>
+	<p>Average time per day watching tutorials: <span class="highlight bg-green-300 px-1">{stats.averageDurationPerDay}</span></p>
 	<p>Total time watching tutorials: <span class="highlight">{stats.totalDuration}</span></p>
 	<p>Number of flashcards recorded: <span class="highlight">{flashcards.length}</span></p>
 	<ul class="mt-3">
 		{#each stats.timeUnits as timeUnit}
-			<li>{timeUnit.calendarDate} - {timeUnit.duration}</li>
+			<li class="{timeUnit.duration === '00:00:00' ? 'text-red-700' : 'text-green-700'}">{timeUnit.calendarDate} - {timeUnit.duration}</li>
 		{/each}
 	</ul>
 </section>
