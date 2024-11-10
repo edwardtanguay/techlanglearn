85783c3d0db555aacd90a73c5f9e7392c0d454f2
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-10 12:29:29 +0100

    console runs on load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 	import FlashcardArea from '../components/FlashcardArea.svelte';
 	import StatsArea from '../components/StatsArea/StatsArea.svelte';
+
+	onMount(() => {
+		console.log('111134', 'loaded');
+	})
 </script>
 
 <FlashcardArea />
