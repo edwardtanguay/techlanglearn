8281af7ae40543cc405af18da0731608eb1f49b8
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-23 16:05:28 +0100

    before move

---
+++ backend/scripts/parsedata/buildManager.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func build(directory string) {
+	fmt.Printf("directory is: %s\n", directory)
+}
--- backend/scripts/parsedata/main.go
+++ backend/scripts/parsedata/main.go
@@ -12,6 +12,7 @@ func main() {
 		Use:   "parsedata",
 		Short: "Reads the .md files and parses them into JSON files that contain data from the md files, plus extended HTML to be displayed on the site.",
 		Run: func(cmd *cobra.Command, args []string) {
+			build("../../../" + config.TutorialFilesDirectory)
 			parse("../../../" + config.TutorialFilesDirectory)
 		},
 	}
