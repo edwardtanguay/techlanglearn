5daaa09c665d75ae395bed8c6196b71010c623d8
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-23 16:14:56 +0100

    showing lines

--- backend/scripts/parsedata/buildManager.go
+++ backend/scripts/parsedata/buildManager.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func build(directory string) {
-	fmt.Printf("directory is: %s\n", directory)
+	mdPathAndFileNames, _ := getFilesFromDirectory(directory, "tts.txt")
+	buildTutorials(mdPathAndFileNames)
+}
+
+func buildTutorials(mdPathAndFileNames []string) error {
+	for _, mdPathAndFileName := range mdPathAndFileNames {
+		lines := getLinesFromFile(mdPathAndFileName)
+		for _, line := range lines {
+			fmt.Printf("LINE: %s\n", line)
+		}
+	}
+	return nil
 }
