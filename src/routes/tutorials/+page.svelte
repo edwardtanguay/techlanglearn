e34e0e51bb4d352b3c42768c061bc2d9ed2c0c26
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-16 19:25:38 +0100

    styles in

--- src/routes/tutorials/+page.svelte
+++ src/routes/tutorials/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { getStore } from '../../store.svelte';
+	import './styles.scss';
 
 	const store = getStore();
 </script>
 
 <header>
 	<h1 class="main">Tutorials</h1>
-	<p>There are {store.tutorials.length} tutorials.</p>
+	<p class="test123">There are {store.tutorials.length} tutorials.</p>
+	{#each store.tutorials as tutorial}
+		<p>{tutorial.title}</p>
+	{/each}
 </header>
