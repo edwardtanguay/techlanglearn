5d816464f3097778ab3929209b199b0ce058dbc1
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-18 02:20:47 +0100

    finished

--- src/routes/flashcards/+page.svelte
+++ src/routes/flashcards/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import { getStore } from '../../store.svelte';
+	import type { Flashcard } from '../../types';
 	import './styles.scss';
 
 	const store = getStore();
+
+	const handleToggleFlashcard = (flashcard: Flashcard) => {
+		flashcard.isOpen = !flashcard.isOpen;
+	}
 </script>
 
 <div class="area_flashcard">
@@ -10,7 +15,9 @@
 
 	{#each store.flashcards as flashcard}
 		<div class="mb-6 w-fit">
-			<div class={`front rounded-t-md bg-slate-400 px-3 py-2 front-lang-${flashcard.language}`}>
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<div on:click={() => handleToggleFlashcard(flashcard)} class={`cursor-pointer front rounded-t-md bg-slate-400 px-3 py-2 front-lang-${flashcard.language}`}>
 				{flashcard.front}
 			</div>
 			{#if flashcard.isOpen}
